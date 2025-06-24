af634da4dd1197569cbabc68e85b973f13a88894
amdkholil <amdkholil@gmail.com>
2025-06-24 23:12:49 +0700

    Tue Jun 24 11:12:49 PM WIB 2025

---
+++ database/migrations/2025_06_24_151015_create_menus_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->nullable()->index();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->integer('price');
+            $table->string('image')->nullable();
+            $table->boolean('available')->default(true);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('menus');
+    }
+};
---
+++ database/migrations/2025_06_24_152830_create_order_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('order_id')->index();
+            $table->foreignId('menu_id')->index();
+            $table->integer('quantity');
+            $table->integer('price');
+            $table->string('note')->nullable();
+            $table->bigInteger('option_id')->nullable()->index();
+            $table->string('option_name')->nullable();
+            $table->bigInteger('option_value_id')->nullable();
+            $table->string('option_value_value')->nullable();
+            $table->integer('option_value_price')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('order_items');
+    }
+};
