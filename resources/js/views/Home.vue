abf9503940f55afbcf15594fb98870e831d97997
amdkholil <amdkholil@gmail.com>
2025-06-26 10:16:31 +0700

    'date'

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <p>Counter: {{ counter }}</p>
+    <button @click="increment">Tambah</button>
+  </div>
+</template>
+
+<script setup>
+import { useMainStore } from '../store'
+const { counter, increment } = useMainStore()
+</script>
